0a38b6d1272a4d923e6c187acb03851e339a9d03
souravachar <125204544+souravachar@users.noreply.github.com>
2023-09-06 12:24:17 +0530

    Add files via upload
    basic's of Javascript

---
+++ class-4/example.js
@@ -0,0 +1,25 @@
+let a = 16;
+var result="true";
+if(a<15)
+{
+  result="true"
+}
+else{
+  result="false"
+}
+console.log(result)
+
+
+function smallerThan(b,c)
+{
+    if(b<c)
+    console.log("lesser than")
+}
+
+smallerThan(80,90)
+
+function display(name)
+{
+    const message="hello"($name);
+    console.log(message)
+}
